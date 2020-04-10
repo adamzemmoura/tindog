a6e6bbe443bdca434062bb2c8ee58085d6ed9a7a
Adam Zemmoura <adamzemmoura@gmail.com>
2020-04-10 13:20:13 +0200

    update dogs/random to retrieve all dogs in Firestore collection and return json response

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,28 @@
-const functions = require('firebase-functions');
 const express = require('express');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp();
+let db = admin.firestore();
 
 const app = express();
 
 app.get('/dogs/random', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  // TODO: retrieve random dog from Firestore and send JSON response
-  res.send('random cached dog');
+
+  db.collection('dogs')
+    .get()
+    .then( (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+      res.json(snapshot);
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+      res.status(500).json({ error: 'Error getting documents' });
+    });
+
 });
 
 exports.app = functions.https.onRequest(app);
